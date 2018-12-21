660bf932e55d6278d37923ec9dd155a86152ad8e
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-12-20 19:31:12 -0500

    just need the comparing logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,83 +2,74 @@
 let friendsList = [
     {
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+    "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "q1": '2',
+    "q2": '4',
+    "q3": '2',
+    "q4": '3',
+    "q5": '3',
+    "q6": '4',
+    "q7": '2',
+    "q8": '1 (Strongly Disagree)',
+    "q9": '3',
+    "q10": '3'
 },
 {
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+    "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "q1": '4',
+    "q2": '3',
+    "q3": '3',
+    "q4": '4',
+    "q5": '2',
+    "q6": '2',
+    "q7": '4',
+    "q8": '1 (Strongly Disagree)',
+    "q9": '3',
+    "q10": '3'
 },
 {
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+    "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "q1": '4',
+    "q2": '2',
+    "q3": '2',
+    "q4": '4',
+    "q5": '3',
+    "q6": '3',
+    "q7": '4',
+    "q8": '1 (Strongly Disagree)',
+    "q9": '2',
+    "q10": '3'
 },
 {
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+    "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "q1": '4',
+    "q2": '3',
+    "q3": '2',
+    "q4": '2',
+    "q5": '3',
+    "q6": '2',
+    "q7": '4',
+    "q8": '1 (Strongly Disagree)',
+    "q9": '2',
+    "q10": '3'
 },
 {
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}];
+    "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "q1": '4',
+    "q2": '2',
+    "q3": '3',
+    "q4": '4',
+    "q5": '3',
+    "q6": '2',
+    "q7": '2',
+    "q8": '1 (Strongly Disagree)',
+    "q9": '3',
+    "q10": '4'
+}
+];
 
 module.exports = friendsList;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,29 @@
 var path = require('path');
 var friendsList = require('../app/data/friends');
-module.exports = function(app){
-    app.use("/survey", function(req, res){
+module.exports = function (app) {
+    app.post("/survey", function (req, res) {
         var response = req.body;
+        var friendScore = 0;
+        var compareFriends = function () {
+            for (i = 0; i < friendsList.length; i++) {
+                // console.log(friendsList[i]);
+                
+                for(j=0; j<=10; j++){
+                    let answer = `q${j}`;
+                if (response.answer == friendsList[i].answer) {
+                    console.log(response.answer);
+                    console.log(friendsList[i].answer);
+                    friendScore++
+                };
+            };
+                console.log(friendScore);
+            
+        };
+        };
         friendsList.push(response);
 
-        console.log(friendsList);
+
         
-    })
+
+    });
 }
